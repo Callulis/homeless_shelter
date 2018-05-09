d503f273289f4b6ec4111489d31aa540037cf5c4
Chris Allulis <Valandi.allulis@gmail.com>
2018-05-09 19:48:23 -0400

    ,.../

--- src/actions/shelterActions.js
+++ src/actions/shelterActions.js
@@ -35,7 +35,7 @@ export const fetchShelters = () => {
 
     dispatch(fetchSheltersRequest());
     // Returns a promise
-    return Axios.get(apiUrl)
+    return Axios.get(apiUrl + "shelter-posts")
                 .then(response => {
                   // dispatch another action
                   // to consume data
