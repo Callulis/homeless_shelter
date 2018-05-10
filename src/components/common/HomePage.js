5a398dab245661cb0c88781551faa5ef31a5d0a8
dinesh333 <dinesh123basnet@gmail.com>
2018-05-10 13:34:58 -0400

    Adding some directions in the homepage

--- src/components/common/HomePage.js
+++ src/components/common/HomePage.js
@@ -1,15 +1,14 @@
 import React from 'react';
+import './SignupLoginPage.css';
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home Page</h1>
-
-      <p>Welcome!</p>
-
-      <p>Thank you for visiting our website!</p>
-
-      <p>Please add whatever a particualr homeless shelter needs!</p>
+    <div id="home">
+      <h1>Welcome!</h1>
+      <h3>Thank you for visiting our website!</h3>
+      <h3>Click on <span id="makeBold">Shelter Posts</span> tab to view all the posts made so far.</h3>
+      <h3>Please <span id="makeBold">Sign Up</span> or <span id="makeBold">Login</span> to make a post about a homless shelter and its needs.</h3>
+      <h3>&nbsp;&nbsp; &nbsp; &nbsp; - Please make sure to contact the shelter to find out its needs if you haven't already.</h3>
     </div>
   );
 }
