35a0b7a049011addc15feba14fe7e6512ff1ff2b
Chris Allulis <Valandi.allulis@gmail.com>
2018-05-09 21:41:33 -0400

    asdf

--- dist/server.js
+++ dist/server.js
@@ -46,7 +46,18 @@ const app = (0, _express2.default)();
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+      // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
 });
 
 app.use(_express2.default.static('uploads'));
